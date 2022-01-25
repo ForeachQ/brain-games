f5087229f2effb1829af69472321958982ff091d
Ilya Golovin <foreachq@gmail.com>
2022-01-25 23:26:14 +0300

    add brain-prime game data provider

---
+++ src/Games/PrimeGameData.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Brain\Games\Games\PrimeGameData;
+
+use function Brain\Games\Prime\isPrime;
+
+function generateData(): array
+{
+    $gameDesription = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+    $answersToWin = 3;
+    $questions = [];
+
+    // generate answers and questions for game
+    for ($i = 0; $i < $answersToWin; $i++) {
+        do {
+            $number = rand(1, 30);
+        } while (in_array($number, array_keys($questions)));
+
+        $answer = isPrime($number) ? 'yes' : 'no';
+        $questions[$number] = $answer;
+    }
+
+    return [$gameDesription, $answersToWin, $questions];
+}
